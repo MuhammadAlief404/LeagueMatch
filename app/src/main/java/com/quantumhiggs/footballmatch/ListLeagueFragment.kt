f47d54fbc97989d5e567d4c47cd8bba21fbdccb7
Muhammad Alief <alief.pribadi@gmail.com>
2019-11-03 16:01:28 +0700

    - finish base layout (XML)

--- app/src/main/java/com/quantumhiggs/footballmatch/ListLeagueFragment.kt
+++ app/src/main/java/com/quantumhiggs/footballmatch/ListLeagueFragment.kt
@@ -2,10 +2,10 @@ package com.quantumhiggs.footballmatch
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -13,10 +13,6 @@ import android.view.ViewGroup
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
 class ListLeagueFragment : Fragment() {
 
     override fun onCreateView(
