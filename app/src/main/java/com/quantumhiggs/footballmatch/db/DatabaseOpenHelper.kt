f80131416774aea2b43fe9b1ccad706fbc99834d
Muhammad Alief <alief.pribadi@gmail.com>
2019-11-12 17:00:29 +0700

    - add Database Helper - add Favorites Data Class

---
+++ app/src/main/java/com/quantumhiggs/footballmatch/db/DatabaseOpenHelper.kt
@@ -0,0 +1,41 @@
+package com.quantumhiggs.footballmatch.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+
+class DatabaseOpenHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "FavoriteMatch.db", null, 1) {
+    companion object {
+        private var instance: DatabaseOpenHelper? = null
+
+        @Synchronized
+        fun getInstance(ctx: Context): DatabaseOpenHelper {
+            if (instance == null) {
+                instance = DatabaseOpenHelper(ctx.applicationContext)
+            }
+            return instance as DatabaseOpenHelper
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.createTable(
+            Favorites.TABLE_FAVORITE, true,
+            Favorites.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+            Favorites.EVENT_ID to TEXT + UNIQUE,
+            Favorites.EVENT_NAME to TEXT,
+            Favorites.DATE_EVENT to TEXT,
+            Favorites.HOME_NAME to TEXT,
+            Favorites.AWAY_NAME to TEXT,
+            Favorites.HOME_SCORE to TEXT,
+            Favorites.AWAY_SCORE to TEXT
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.dropTable(Favorites.TABLE_FAVORITE, true)
+    }
+}
+
+// Access property for Context
+val Context.database: DatabaseOpenHelper
+    get() = DatabaseOpenHelper.getInstance(applicationContext)
---
+++ app/src/main/java/com/quantumhiggs/footballmatch/db/Favorites.kt
@@ -0,0 +1,25 @@
+package com.quantumhiggs.footballmatch.db
+
+data class Favorites(
+    val id: Double?,
+    val eventId: String?,
+    val eventName: String?,
+    val dateEvent: String?,
+    val homeName: String?,
+    val awayName: String?,
+    val homeScore: String?,
+    val awayScore: String?
+) {
+    companion object {
+        const val TABLE_FAVORITE: String = "TABLE_FAVORITE"
+        const val ID: String = "ID_"
+        const val EVENT_ID: String = "EVENT_ID"
+        const val EVENT_NAME: String = "EVENT_NAME"
+        const val DATE_EVENT: String = "DATE_EVENT"
+        const val HOME_NAME: String = "HOME_NAME"
+        const val AWAY_NAME: String = "AWAY_NAME"
+        const val HOME_SCORE: String = "HOME_SCORE"
+        const val AWAY_SCORE: String = "AWAY_SCORE"
+
+    }
+}
